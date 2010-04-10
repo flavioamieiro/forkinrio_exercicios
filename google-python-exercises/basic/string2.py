3a5a7851ea1f4fc471bc790f685e90077929ad3c
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-04-09 21:07:12 -0300

    verbing da lista string2 do Google

--- google-python-exercises/basic/string2.py
+++ google-python-exercises/basic/string2.py
@@ -16,8 +16,14 @@
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
 def verbing(s):
-  # +++your code here+++
-  return
+  if len(s) < 3:
+    return s
+
+  if s[-3:] == 'ing':
+    return '%sly' % s
+
+  else:
+    return '%sing' % s
 
 
 # E. not_bad
