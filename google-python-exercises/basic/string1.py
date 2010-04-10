3f2eef948c5ea5bd3a66e19e5bb2ecd376211a3d
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-04-09 21:02:16 -0300

    mix_up da lista string1 do Google

--- google-python-exercises/basic/string1.py
+++ google-python-exercises/basic/string1.py
@@ -57,8 +57,7 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+    return '%s%s %s%s' % (b[:2], a[2:], a[:2], b[2:])
 
 
 # Provided simple test() function used in main() to print
