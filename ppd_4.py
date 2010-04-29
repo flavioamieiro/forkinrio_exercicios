0328298ba71a9cadd125412699c9469675d81772
Flavio Amieiro <amieiro.flavio@gmail.com>
2010-04-28 23:28:30 -0300

    Exercício 2 da parte IV do livro Python para Desenvolvedores

--- ppd_4.py
+++ ppd_4.py
@@ -23,6 +23,30 @@ class Quadrado(object):
         return self.lado ** 2
 
 
+class Lista(list):
+    def sem_repeticao(self):
+        """
+        >>> lista = Lista((1, 2, 3, 2))
+        >>> lista.sem_repeticao()
+        [1, 2, 3]
+        """
+        #Na marreta vale?
+        return list(set(self))
+
+    def sem_repeticao_nem_marreta(self):
+        """
+        >>> lista = Lista((1, 2, 3, 2))
+        >>> lista.sem_repeticao_nem_marreta()
+        [1, 2, 3]
+        """
+        resultado = []
+        for value in self:
+            if value not in resultado:
+                resultado.append(value)
+        return resultado
+
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
